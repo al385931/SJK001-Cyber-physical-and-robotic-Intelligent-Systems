68a1d4978741244642dfae15d6ed6ccc00397a3a
Raquel Lázaro Belenguer <147169007+al385931@users.noreply.github.com>
2023-10-31 18:52:42 +0100

    First version

---
+++ racing-challenge.py
@@ -0,0 +1,11 @@
+from GUI import GUI
+from HAL import HAL
+# Enter sequential code!
+
+while True:
+    # Enter iterative code!
+    img = HAL.getImage()
+    GUI.showImage(img)
+    print(i)
+    i=i+1
+    
