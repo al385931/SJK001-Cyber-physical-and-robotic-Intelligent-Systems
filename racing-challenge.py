3a593a2a20d8b8b2c4c3b5703773f079391c5277
Raquel Lázaro Belenguer <147169007+al385931@users.noreply.github.com>
2023-10-31 19:19:25 +0100

    V2 racing-challenge.py

--- racing-challenge.py
+++ racing-challenge.py
@@ -1,11 +1,20 @@
 from GUI import GUI
 from HAL import HAL
+import cv2
 # Enter sequential code!
 
+i=0
 while True:
     # Enter iterative code!
     img = HAL.getImage()
-    GUI.showImage(img)
+    
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv,
+                      (0, 125, 125),
+                      (30, 255, 255))
+    
+
+    GUI.showImage(red_mask)
     print(i)
     i=i+1
     
