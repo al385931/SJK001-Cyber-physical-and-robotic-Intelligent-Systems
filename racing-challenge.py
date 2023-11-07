9d64c59a8bc0e884ef6a5a6ace26c7d03858b9ce
Raquel Lázaro Belenguer <147169007+al385931@users.noreply.github.com>
2023-11-07 18:48:04 +0100

    v3 racing-challenge.py

--- racing-challenge.py
+++ racing-challenge.py
@@ -13,8 +13,20 @@ while True:
                       (0, 125, 125),
                       (30, 255, 255))
     
+    contours, hierarchy = cv2.findContours(red_mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    M  = cv2.moments(contours[0])
 
-    GUI.showImage(red_mask)
-    print(i)
-    i=i+1
+
+    if M["m00"] != 0:
+        cX = M["m10"] / M["m00"]
+        cY = M["m01"]/ M["m00"]
+    else:
+        cX, cY = 0,0
+    if cX > 0:
+        err = 320 -cX
+        HAL.setV(1)
+        HAL.setW(0.01*err)
     
+    GUI.showImage(red_mask)
+    print('%d cX: %.2f cy: %.2f' % (i,cX, cY))
+    i = i+1
